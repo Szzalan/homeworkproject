36d9c644a662ce1a86499d76cba6b6eb919fc8d5
Szilágyi_Zalán <zalanka12345678@gmail.com>
2023-05-19 23:29:25 +0200

    Finished implementing game model

--- src/main/java/tablegame/model/TableGameModel.java
+++ src/main/java/tablegame/model/TableGameModel.java
@@ -4,32 +4,60 @@ import java.util.ArrayList;
 
 public class TableGameModel {
 
-    private static final int BOARD_HORIZONTAL = 7;
-    private static final int BOARD_VERTICAL = 6;
-    State[][] table = new State[BOARD_HORIZONTAL][BOARD_VERTICAL];
+    private static final int BOARD_ROWS = 6;
+
+
+
+    private static final int BOARD_COLS = 7;
+    State[][] table = new State[BOARD_ROWS][BOARD_COLS];
+
+    private State currentPlayer = State.BLUE;
 
     public TableGameModel() {
         fillTable();
     }
+    public State[][] getTable() {
+        return table;
+    }
+    public TableGameModel(State[][] table) {
+        this.table = table;
+    }
     private void fillTable(){
-        for (int i = 0;i < BOARD_HORIZONTAL;i++) {
-            for (int j = 0;j < BOARD_VERTICAL;j++){
+        for (int i = 0; i < BOARD_ROWS; i++) {
+            for (int j = 0; j < BOARD_COLS; j++){
                 setState(i,j);
             }
         }
     }
-    private State goalCheck(){
-        if(noMovesLeft(State.RED) || noMorePieces(State.RED)){
+
+    public State getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void changeCurrentPlayer() {
+        if(currentPlayer == State.BLUE){
+            currentPlayer = State.RED;
+        } else {
+            currentPlayer = State.BLUE;
+        }
+    }
+    public State goalCheck(){
+
+        if(noMorePieces(State.RED)){
+            return State.BLUE;
+        } else if(noMorePieces(State.BLUE)){
+            return State.RED;
+        } else if (noMovesLeft(State.BLUE)){
+            return State.RED;
+        } else if (noMovesLeft(State.RED)){
             return State.BLUE;
         }
-        if(noMovesLeft(State.BLUE) || noMorePieces(State.BLUE)){
-            return State.RED;
-        }
+
         return State.NONE;
     }
     private boolean noMovesLeft(State player){
-        for (int i = 0;i < BOARD_HORIZONTAL;i++) {
-            for (int j = 0; j < BOARD_VERTICAL; j++) {
+        for (int i = 0; i < BOARD_ROWS; i++) {
+            for (int j = 0; j < BOARD_COLS; j++) {
                 if(table[i][j] == player){
                     if(getPossibleMoves(i,j).size() != 0)
                         return false;
@@ -39,8 +67,8 @@ public class TableGameModel {
         return true;
     }
     private boolean noMorePieces(State player){
-        for (int i = 0;i < BOARD_HORIZONTAL;i++) {
-            for (int j = 0; j < BOARD_VERTICAL; j++) {
+        for (int i = 0; i < BOARD_ROWS; i++) {
+            for (int j = 0; j < BOARD_COLS; j++) {
                 if(table[i][j] == player)
                     return false;
             }
@@ -49,14 +77,15 @@ public class TableGameModel {
     }
     private void setState(int row,int col){
         if(row == 0) table[row][col] = State.RED;
-        else if (row == 7) table[row][col] = State.BLUE;
-        else if ( (row == 4 && col == 2) || (row == 2 && col == 3) ) table[row][col] = State.BLACK;
+        else if (row == 5) table[row][col] = State.BLUE;
+        else if ( (row == 3 && col == 2) || (row == 2 && col == 4) ) table[row][col] = State.BLACK;
         else table[row][col] = State.NONE;
 
     }
-    private ArrayList<Position> getPossibleMoves(int row, int col){
+    public ArrayList<Position> getPossibleMoves(int row, int col){
         ArrayList<Position> steps = new ArrayList<>();
         int direction = table[row][col] == State.RED ? 1 : -1;
+        System.out.println(table[row][col]);
         Position currentPosition = new Position(row,col);
 
         checkPosition(steps,currentPosition,new Position(row+direction,col));
@@ -65,13 +94,13 @@ public class TableGameModel {
 
         return steps;
     }
-    private void move(Position start,Position target){
+    public void move(Position start,Position target){
         table[target.row()][target.col()] = table[start.row()][start.col()];
         table[start.row()][start.col()] = State.NONE;
     }
     private void checkPosition(ArrayList<Position> steps,Position start,Position target) {
-        if((target.row() >= 0 && target.row() < BOARD_HORIZONTAL) &&
-        (target.col() >= 0 && target.col() < BOARD_VERTICAL)) {
+        if((target.row() >= 0 && target.row() < BOARD_ROWS) &&
+        (target.col() >= 0 && target.col() < BOARD_COLS)) {
             State startState = table[start.row()][start.col()];
             State targetState = table[target.row()][target.col()];
 
